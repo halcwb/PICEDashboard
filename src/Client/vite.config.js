5a1e19fcd664bd8a992a5c9bce3bbc869aa3999d
Casper Bollen <halcwb@Caspers-MacBook-Pro.local>
2024-12-24 12:46:46 +0100

    chore: working on infrastructure

--- src/Client/vite.config.js
+++ src/Client/vite.config.js
@@ -1,13 +1,6 @@
-import path from "node:path"
-import { fileURLToPath } from "node:url"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fable from "vite-plugin-fable"
 import Inspect from "vite-plugin-inspect"
-
-const root = path.dirname(fileURLToPath(import.meta.url));
-const fsproj = path.resolve(root, "./Client.fsproj");
-console.log("transpilling fsproj", fsproj);
 
 const proxyPort = process.env.SERVER_PROXY_PORT || "8085";
 const proxyTarget = "http://localhost:" + proxyPort;
@@ -15,7 +8,7 @@ console.log("proxying to", proxyTarget);
 
 // https://vite.dev/config/
 export default defineConfig({
-  root : root,
+  root : "dist",
   server: {
     proxy: {
         // redirect requests that start with /api/ to the server on port 8085
@@ -27,7 +20,6 @@ export default defineConfig({
   },
   plugins: [
     Inspect(),
-    fable({ fsproj: fsproj, jsx: "automatic" }),
     react({ include: /\.(fs|js|jsx|ts|tsx)$/, jsxRuntime: "automatic" })
   ],
 })
